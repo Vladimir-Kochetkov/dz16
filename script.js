25c74d9a3affb3d6647ef1a103e0040f68e0635c
Vladimir Kochetkov <js.kochetkov@gmail.com>
2022-02-21 01:08:25 +0300

    add(script.js):solution 16 tasks

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+class First {
+    constructor() { }
+    hello() {
+        console.log('Привет я метод родителя!');
+    }
+}
+
+class Second extends First {
+    constructor() {
+        super();
+    }
+    hello() {
+        super.hello();
+        console.log('А я наследуемый метод!');
+    }
+}
+const test = new Second();
+test.hello(); 
